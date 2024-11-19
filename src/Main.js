49ee7d08aae7997a43602eb9d3e6c4da6238004b
daehy <rkdeown10@naver.com>
2024-11-20 02:22:51 +0900

    메인페이지

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import styles from "./Main.module.css";
+
+function Main() {
+  return (
+    <div className={styles.container}>
+      <img className={styles.left} src="./image/LEFT.png" alt="Left"/>
+      <div className={styles.text}>
+        <div className={styles.Big}>마피아 42</div>
+        <div className={styles.Small}>대현닷컴 에서 쉽고 간단하게</div>
+        <button className={styles.button}>시작하기</button>
+      </div>
+      <img className={styles.right} src="./image/RIGHT.png" alt="Right"/>
+    </div>
+  );
+}
+
+export default Main;
