b5c3018972ae06e8a2ca0e461a688bcd12c8c8ee
daehy <rkdeown10@naver.com>
2024-11-20 12:42:24 +0900

    fig bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
-=======
->>>>>>> 49ee7d08aae7997a43602eb9d3e6c4da6238004b
 import Main from './Main';
 import Footer from './Footer';
 
 function App() {
   return (
-<<<<<<< HEAD
     <><Routes>
       <Route path="/" element={<Main />} />
       {/* <Route path="/footer" element={<Footer />} /> */}
@@ -15,12 +11,6 @@ function App() {
     
     </Routes>
     <Footer /></>
-=======
-    <>
-    <Main />
-    <Footer />
-    </>
->>>>>>> 49ee7d08aae7997a43602eb9d3e6c4da6238004b
   );
 }
 
