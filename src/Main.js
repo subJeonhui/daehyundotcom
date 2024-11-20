d3c12037cb8d855d6c821702ca75966f9ab1993a
daehy <rkdeown10@naver.com>
2024-11-20 13:11:35 +0900

    fix font

--- src/Main.js
+++ src/Main.js
@@ -7,10 +7,10 @@ function Main() {
 
     <div className={styles.container}>
       <img className={styles.left} src="./image/LEFT.png" alt="LEFT"/>
-
+      
       <div className={styles.text}>
-        <div className={styles.Big}>마피아 42</div>
-        <div className={styles.Small}>대현닷컴 에서 쉽고 간단하게</div>
+        <div className={styles.Big}>대현닷컴</div>
+        <div className={styles.Small}>마피아42를 쉽고 간단하게</div>
         <button className={styles.button}>시작하기</button>
       </div>
       <img className={styles.right} src="./image/RIGHT.png" alt="Right"/>
