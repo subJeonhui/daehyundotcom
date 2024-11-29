6b860774e7fcbf95d1d222dcdcc7006dd8173f98
daehyuh <rkdeown10@naver.com>
2024-11-29 15:52:48 +0900

    change get Path

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
-function Header({ setSelectedMenu }) {
-    const [selected, setSelected] = useState(null); // 메뉴 선택 상태
-
-    // 메뉴 클릭 시 상태 업데이트
-    const handleMenuClick = (menuName) => {
-        setSelected(menuName);  // 메뉴 이름을 상태로 저장
-        setSelectedMenu(menuName); // 부모 컴포넌트로 메뉴 이름 전달
-    };
+function Header() {
+    const {pathname} = useLocation();
+    const path = decodeURIComponent(pathname);
+    console.log(path);
     
     return (
+        
         <div className={styles.menu}>
+            {path}
             <div className={styles.header}>
                 <Link to="/" className={styles.notactive}>
                     <img className={styles.logo} src="./image/logo.png" alt="LOGO" />
@@ -22,55 +20,48 @@ function Header({ setSelectedMenu }) {
             <div className={styles.menubar}>
                 <Link
                     to="/상자깡"
-                    className={selected === '상자깡' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('상자깡')}
+                    className={path === '/상자깡' ? styles.active : styles.notactive}
                 >
                     상자깡 확률
                 </Link>                
                 <Link
                     to="/티어"
-                    className={selected === '티어' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('티어')}
+                    className={path === '/티어' ? styles.active : styles.notactive}
                 >
                     티어 계산기
                 </Link>
 
                 <Link
                     to="/우체통"
-                    className={selected === '우체통' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('우체통')}
+                    className={path === '/우체통' ? styles.active : styles.notactive}
                 >
                     우체통 계산기
                 </Link>
 
                 <Link
                     to="/환율"
-                    className={selected === '환율' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('환율')}
+                    className={path === '/환율' ? styles.active : styles.notactive}
                 >
                     환율 계산기
                 </Link>
 
                 <Link
                     to="/권엽"
-                    className={selected === '권엽' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('권엽')}
+                    className={path === '/권엽' ? styles.active : styles.notactive}
                 >
                     권엽 계산기
                 </Link>
                 
                 <Link
                     to="/출석보상"
-                    className={selected === '출석보상' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('출석보상')}
+                    className={path === '/출석보상' ? styles.active : styles.notactive}
                 >
                     출석보상 계산기
                 </Link>
 
                 <Link
                     to="/유저게시판"
-                    className={selected === '유저게시판' ? styles.active : styles.notactive}
-                    onClick={() => handleMenuClick('유저게시판')}
+                    className={path === '/유저게시판' ? styles.active : styles.notactive}
                 >
                     유저게시판
                 </Link>
@@ -88,6 +79,7 @@ function Header({ setSelectedMenu }) {
                 >
                     추석이벤트 연습 사이트
                 </Link> */}
+                
             </div>
         </div>
     );
