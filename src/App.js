4966ebfa700100a9bcc934e584f8c87819a6c42e
daehyuh <rkdeown10@naver.com>
2024-11-29 09:59:00 +0900

    del react-ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import ReactGA from "react-ga";
 import Main from './Main';
 import Footer from './Footer';
 import Tier from './Tier';
@@ -14,39 +13,32 @@ import Gacha from "./Gacha";
 import DailyReward from "./DailyReward";
 import { Analytics } from '@vercel/analytics/react';
 
-const TRACKING_ID = "G-5VFJNJPHTE"; // Google Analytics Tracking ID
-
 function App() {
   const location = useLocation(); // 현재 경로 정보를 가져옴
   const [selectedMenu, setSelectedMenu] = useState(null); // 선택된 메뉴 상태
 
-  const showHeader = location.pathname !== '/'; // "/" 경로에서 Header를 렌더링하지 않음
 
-  useEffect(() => {
-    // Google Analytics 초기화
-    ReactGA.initialize(TRACKING_ID);
 
-    // 페이지 변경 시마다 Google Analytics에 페이지 뷰를 기록
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
+  const showHeader = location.pathname !== '/'; // "/main"일 때 Header를 렌더링하지 않도록 설정
 
   return (
     <>
-      {showHeader && <Header setSelectedMenu={setSelectedMenu} />} {/* "/" 경로에서는 Header를 렌더링하지 않음 */}
-
+      {showHeader &&  <Header setSelectedMenu={setSelectedMenu} />} {/* /main 경로에서는 Header를 렌더링하지 않음 */}
+      
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/티어" element={<Tier />} />
+        <Route path="/티어"  element={<Tier />} />
         <Route path="/우체통" element={<Mail />} />
         <Route path="/환율" element={<Exchange />} />
-        <Route path="/권엽" element={<Discipline />} />
-        <Route path="/상자깡" element={<Gacha />} />
-        <Route path="/출석보상" element={<DailyReward />} />
-        <Route path="/유저게시판" element={<Board />} />
+        <Route path="/권엽"  element={<Discipline />} />
+        <Route path="/상자깡"  element={<Gacha />} />
+        <Route path="/출석보상"  element={<DailyReward />} />
+        <Route path="/유저게시판"  element={<Board />} />
         <Route path="/유저게시판/:id" element={<BoardDetail />} />
       </Routes>
       <Footer />
       <Analytics />
+      <></>
     </>
   );
 }
